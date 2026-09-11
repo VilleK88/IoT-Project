ab8221baeb42c5797041e8ff8e3d06cb2acab388
ville <ville.karppinen88@gmail.com>
2026-09-11 10:27:50 +0300

    deleted obsolete comment

--- firmware/main.py
+++ firmware/main.py
@@ -11,7 +11,7 @@ import gc
 
 async def main():
     # Garbage-collection threshold for the full system.
-    gc.threshold(5_000_000) # 5_000_000
+    gc.threshold(5_000_000)
 
     file_manager = FileManager()
     log_manager = LogManager(file_manager)
